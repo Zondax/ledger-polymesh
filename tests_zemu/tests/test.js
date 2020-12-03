a7d219bd27e0e142c663c616c4094e87436fb6be
ER <raynaudo.ee@gmail.com>
2020-12-03 17:22:12 -0300

    Fix zemu tests

--- tests_zemu/tests/test.js
+++ tests_zemu/tests/test.js
@@ -16,7 +16,7 @@
 
 import jest, {expect} from "jest";
 import Zemu from "@zondax/zemu";
-const {newKusamaApp} = require("@zondax/ledger-polkadot");
+const {newPolymathApp} = require("@zondax/ledger-polkadot");
 
 const ed25519 = require("ed25519-supercop");
 import {blake2bFinal, blake2bInit, blake2bUpdate} from "blakejs";
@@ -48,7 +48,7 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = newKusamaApp(sim.getTransport());
+            const app = newPolymathApp(sim.getTransport());
             const resp = await app.getVersion();
 
             console.log(resp);
@@ -68,7 +68,7 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = newKusamaApp(sim.getTransport());
+            const app = newPolymathApp(sim.getTransport());
 
             const resp = await app.getAddress(0x80000000, 0x80000000, 0x80000000);
 
@@ -77,8 +77,8 @@ describe('Basic checks', function () {
             expect(resp.return_code).toEqual(0x9000);
             expect(resp.error_message).toEqual("No errors");
 
-            const expected_address = "JMdbWK5cy3Bm4oCyhWNLQJoC4cczNgJsyk7nLZHMqFT7z7R";
-            const expected_pk = "ffbc10f71d63e0da1b9e7ee2eb4037466551dc32b9d4641aafd73a65970fae42";
+            const expected_address = "2DtHHz6WwfxB7JMzZ7mMw3cxzffBrvU9xTgqYqsaP4EEUGzD";
+            const expected_pk = "3f94b432d8db17738656f93bb685094c3ad308c9607ef4a8807b6930a1304fb4";
 
             expect(resp.address).toEqual(expected_address);
             expect(resp.pubKey).toEqual(expected_pk);
@@ -92,7 +92,7 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = newKusamaApp(sim.getTransport());
+            const app = newPolymathApp(sim.getTransport());
 
             const respRequest = app.getAddress(0x80000000, 0x80000000, 0x80000000, true);
             // Wait until we are not in the main menu
@@ -106,8 +106,8 @@ describe('Basic checks', function () {
             expect(resp.return_code).toEqual(0x9000);
             expect(resp.error_message).toEqual("No errors");
 
-            const expected_address = "JMdbWK5cy3Bm4oCyhWNLQJoC4cczNgJsyk7nLZHMqFT7z7R";
-            const expected_pk = "ffbc10f71d63e0da1b9e7ee2eb4037466551dc32b9d4641aafd73a65970fae42";
+            const expected_address = "2DtHHz6WwfxB7JMzZ7mMw3cxzffBrvU9xTgqYqsaP4EEUGzD";
+            const expected_pk = "3f94b432d8db17738656f93bb685094c3ad308c9607ef4a8807b6930a1304fb4";
 
             expect(resp.address).toEqual(expected_address);
             expect(resp.pubKey).toEqual(expected_pk);
@@ -120,12 +120,12 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = newKusamaApp(sim.getTransport());
+            const app = newPolymathApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "0400d401b48506d4de473a04f40ad8114add5eda886068a71efdf95eee616ee4909e33158139ae28a3dfaac5fe1560a5e9e05cd503ae1103006d0fe707000003000000b0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafeb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe";
+            let txBlobStr = "000009612869d50391018ed73e0dd107000001000000d561a98b341d86c642c5ac5a8603fc537c136c38472e451c73229a372197cfacd561a98b341d86c642c5ac5a8603fc537c136c38472e451c73229a372197cfac";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -137,7 +137,7 @@ describe('Basic checks', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "sign_basic", 13);
+            await sim.compareSnapshotsAndAccept(".", "sign_basic", 10);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
@@ -163,12 +163,12 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = newKusamaApp(sim.getTransport());
+            const app = newPolymathApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "060540632751e57bd704b9d0e1a016f35e86df929bac522e6cfc4eae03eb2a4411862ff768f8cd08f228b69a8bd71da4caa9611fcfc1be95df68cc1a28e45d7f8e4e38b80d7731115479b1bd9918a1294bd9d22c62f7de1bbe2553525dd52a779b3110ff28e16b81b22bbc3d3163b3ba1fefb9438cea8560cb0711308ab398351c89464eb2015e9a9082ac9eecd9d08868b7c38a1ed3eee6625ac027c83c49c3d57064de8952adef2e842465ec59eca2c83574a158640b294e539eae2b64c3fe86b1c7432f90c11bde3958ed8cfe29e26cd6d951d4047a4245e608743be02e7cca6c28d7d91d06e16880c2dc29b7d6d79255048a78212c76436b1fbfe96e9a61be9f0438131d928b065389b2c7eacf4ba6d3599ea01f7e142e48391e784e5603a88c53f1559a8bac1a6969efb9d30b3fad16b8d361a286072b443167a588363bd1146177d8eb34473d68f27d638321d6c35942057253842d9b9770c574dbb7b0a9ce6cac2bd27d0692ebf2d661f587354b41a8b47bce6240dc76a0f802a22930ca77814b79a4c8b3a02ae3b6bffed9ae90b186ceb8976f65dba2cd278c80170d447f49eab42ba84f4f319580c8e5dd0561df82f00d6178fc0661a12033eccdafae1a98c755a79b7622993005a2877a797c968ebd895fa9fab4e5e6bb88e87fb9ebfc0674741994dd6f5c40fb06adc78b9f53ae1cdcb369c77a528ea6ee2a140064783fd50391018ed73e0de707000003000000b0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafeb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe";
+            let txBlobStr = "090060b99a68869326e6fc07525dd343f61fa41758169c28cd9d8cf6a777514faff52c3eba74753062d6cefbeea096f3281e61ef7842403723da5a3234c29189afeece84b9d4038b5d490ceb885d0d4fcedc59234cb34dd0cf9eb9cce79d2c157464f601b26bdd489327ee761bd18620a7d036c8161535d6a4c2fe105ddf117c289f0102596125743536eeca69cc77b12286cd71b8d64ac56d316374e2ec98b8d5763c304fbeabbb80a50b5e9108dd849ffa45d26a80d0a0cfe2fbb893a2649a3ff303f1228ae387bd76994909da1dd73810dc8688a0fd898fc3d175f2132cd93c942b50983125698b3aa06cea209a3aaec6ab1b15159a7b7c657d8760262af970895ad1d5038d248ed73e0dd107000001000000d561a98b341d86c642c5ac5a8603fc537c136c38472e451c73229a372197cfacd561a98b341d86c642c5ac5a8603fc537c136c38472e451c73229a372197cfac";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -180,7 +180,7 @@ describe('Basic checks', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "sign_large_nomination", 41);
+            await sim.compareSnapshotsAndAccept(".", "sign_large_nomination", 25);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
