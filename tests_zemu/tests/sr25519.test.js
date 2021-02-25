1f310c08bb03d068018b6f8130b5a1438ce8b799
Juan Leni <lenijuan@gmail.com>
2021-02-25 13:50:10 +0100

    update sr25519 related testcase / snapshots

--- tests_zemu/tests/sr25519.test.js
+++ tests_zemu/tests/sr25519.test.js
@@ -16,31 +16,34 @@
 
 import jest, {expect} from "jest";
 import Zemu from "@zondax/zemu";
-
-const {newPolkadotApp} = require("@zondax/ledger-polkadot");
 import {blake2bFinal, blake2bInit, blake2bUpdate} from "blakejs";
 var addon = require('../../tests_tools/neon/native');
 
+const {newPolymeshApp} = require("@zondax/ledger-polkadot");
 const Resolve = require("path").resolve;
 
-const APP_PATH = Resolve("../app/output/app_sr25519.elf");
+const APP_PATH_S = Resolve("../app/output/app_sr25519.elf");
 const APP_SEED = "equip will roof matter pink blind book anxiety banner elbow sun young"
 
-const simOptions = {
+var simOptions = {
     logging: true,
     start_delay: 3000,
     custom: `-s "${APP_SEED}"`,
     X11: true
 };
 
+let models = [
+    ['S', {model: 'nanos', prefix: 'S', path: APP_PATH_S}],
+]
+
 jest.setTimeout(60000)
 
 describe('SR25519', function () {
-    test('get address sr25519', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('get address (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
 
             const resp = await app.getAddress(0x80000000, 0x80000000, 0x80000000, false, 1);
 
@@ -49,8 +52,8 @@ describe('SR25519', function () {
             expect(resp.return_code).toEqual(0x9000);
             expect(resp.error_message).toEqual("No errors");
 
-            const expected_address = "1b8tiTYdzk8hZ6W65ppiGPA5TnYBAnoRDSoyxyVbj1DZENm";
-            const expected_pk = "1a08e8cba45e59c761ebe72133da0b7f4de8ce6a263690b07e3bd56dcc8d2226";
+            const expected_address = "2HM5HBpdm3nuMze95qGWPYNypPvhtTbxDwCdLCBXx5zadMSS";
+            const expected_pk = "d8b89698f0deb2b2023ec1dd1a2ca06f01c4db6114648adb02e18dfd12dae77c";
 
             expect(resp.address).toEqual(expected_address);
             expect(resp.pubKey).toEqual(expected_pk);
@@ -60,17 +63,17 @@ describe('SR25519', function () {
         }
     });
 
-    test('show address sr25519', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('show address (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
 
             const respRequest = app.getAddress(0x80000000, 0x80000000, 0x80000000, true, 1);
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-show_address_sr25519", 2);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-show_address_sr25519`, 2);
 
             const resp = await respRequest;
             console.log(resp);
@@ -78,8 +81,8 @@ describe('SR25519', function () {
             expect(resp.return_code).toEqual(0x9000);
             expect(resp.error_message).toEqual("No errors");
 
-            const expected_address = "1b8tiTYdzk8hZ6W65ppiGPA5TnYBAnoRDSoyxyVbj1DZENm";
-            const expected_pk = "1a08e8cba45e59c761ebe72133da0b7f4de8ce6a263690b07e3bd56dcc8d2226";
+            const expected_address = "2HM5HBpdm3nuMze95qGWPYNypPvhtTbxDwCdLCBXx5zadMSS";
+            const expected_pk = "d8b89698f0deb2b2023ec1dd1a2ca06f01c4db6114648adb02e18dfd12dae77c";
 
             expect(resp.address).toEqual(expected_address);
             expect(resp.pubKey).toEqual(expected_pk);
@@ -88,16 +91,17 @@ describe('SR25519', function () {
         }
     });
 
-    test('show address - reject sr25519', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('show address - reject (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
 
             const respRequest = app.getAddress(0x80000000, 0x80000000, 0x80000000, true, 1);
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-            await sim.compareSnapshotsAndAccept(".", "s-show_address_reject_sr25519", 3, 2);
+
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-show_address_reject_sr25519`, 3, 2);
 
             const resp = await respRequest;
             console.log(resp);
@@ -109,16 +113,16 @@ describe('SR25519', function () {
         }
     });
 
-    test('sign basic normal', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('sign basic normal (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "0500c29421760786e979ca1f08f09e1793bcaa031ed77e3ad42dbe173e3cd62b410a33158139ae28a3dfaac5fe1560a5e9e05cd5030003d20296491a0000000500000091b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c391b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+            let txBlobStr = "0000bd494f87d503ae1103008ed73e0ddd0700000500000012fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c712fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c7";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -130,7 +134,7 @@ describe('SR25519', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-sign_basic_normal", 6);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_normal_sr25519`, model === "nanos" ? 3 : 4);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
@@ -153,11 +157,11 @@ describe('SR25519', function () {
         }
     });
 
-    test('sign basic expert', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('sign basic expert (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
@@ -167,7 +171,7 @@ describe('SR25519', function () {
             await sim.clickBoth();
             await sim.clickLeft();
 
-            let txBlobStr = "0500c29421760786e979ca1f08f09e1793bcaa031ed77e3ad42dbe173e3cd62b410a33158139ae28a3dfaac5fe1560a5e9e05cd5030003d20296491a0000000500000091b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c391b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+            let txBlobStr = "24026bc40000000000000127c1210001000800000000000001000800000000000008cce2bd1bf485643a80bcd14091326632501ef80fcff04d9ca08adedc6e65389c0140e2010000000000424e3d8c4553fcf20ef3694464333dcf9c4d3f0c16c47213221d9f37b9b5d5220140e2010000000000544b52415354000000000000a08601000000000000000000000000005992f519733534fed05d2d98ba21c4da22282d39a5a4a725779a5aacccba8b960140e20100000000006cb4e5edb9a93bdb9ab340c8cb7c0ab4001e19264823a0373d2b510a2661ee6a0140e2010000000000544b52415354420000000000a0860100000000000000000000000000d503ae11030033158139ae28a3dfaac5fe1560a5e9e05cdd0700000500000012fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c712fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c7";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -180,7 +184,7 @@ describe('SR25519', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-sign_basic_expert", 12);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_expert_sr25519`, model === "nanos" ? 29 : 30);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
@@ -203,16 +207,16 @@ describe('SR25519', function () {
         }
     });
 
-    test('sign basic - forward/backward', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('sign basic - forward/backward (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "0500c29421760786e979ca1f08f09e1793bcaa031ed77e3ad42dbe173e3cd62b410a33158139ae28a3dfaac5fe1560a5e9e05cd5030003d20296491a0000000500000091b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c391b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+            let txBlobStr = "29000400002c000000d50391010b63ce64c10c05dd0700000500000012fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c712fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c7";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -224,7 +228,7 @@ describe('SR25519', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-sign_basic_FB", 6, 1);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_FB_sr25519`, model === "nanos" ? 4 : 5, 1);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
@@ -247,16 +251,16 @@ describe('SR25519', function () {
         }
     });
 
-    test('sign basic - forward/backward-reject', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('sign basic - forward/backward-reject (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "0500c29421760786e979ca1f08f09e1793bcaa031ed77e3ad42dbe173e3cd62b410a33158139ae28a3dfaac5fe1560a5e9e05cd5030003d20296491a0000000500000091b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c391b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+            let txBlobStr = "29000800002c000000000058000000d503910100dd0700000500000012fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c712fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c7";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -268,7 +272,7 @@ describe('SR25519', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-sign_basic_FB_reject", 7, 1);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_FB_reject_sr25519`, model === "nanos" ? 7 : 8, 1);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
@@ -280,16 +284,16 @@ describe('SR25519', function () {
         }
     });
 
-    test('sign large nomination', async function () {
-        const sim = new Zemu(APP_PATH);
+    test.each(models)('sign large nomination (%s)', async function (_, {model, prefix, path}) {
+        const sim = new Zemu(path);
         try {
             await sim.start(simOptions);
-            const app = newPolkadotApp(sim.getTransport());
+            const app = newPolymeshApp(sim.getTransport());
             const pathAccount = 0x80000000;
             const pathChange = 0x80000000;
             const pathIndex = 0x80000000;
 
-            let txBlobStr = "07054026951b3286754fa7c9138b97e43cf0688b8e7a63edb63a79bcbc624bd534db2764de55acce68b57f853a486692609b81974d730449c272b466b7423752acdc2f3659bd3d1584f3b31261fbb0f9718a3031db1ffd6e15d2cad5e1cc67303802290cf166640c67010f7b253f176449bfb9578b4f545e115e28590a0145beb2be7da0ba634ed63f424e321d4a69aa8c80b9d53733b356e51a9413adc7dd5b56901274856fc73b6f71968133674ddd8fcb1ddc8e26f8e59f45334c14727411db5d3cb0a1c57153b1dcea8783d1eafd6de7f0a039fe9b66c76be7b9e7b95d14923d55ba5db57dcd5e8c54321df0e70670243be1634233ecc5708031efbbafd9e1676cb68cb8c024fdbe85995072874b6f6248da7b603724f225a2ca46319f7f8970688ec7826410f60a71fdd095ad2557f4ea8ea3c49187b502fa8dc753862775107afcebb20575eb7d25efc9b1abf206f1fd8c5f160d9aec935e24546bf265fb0b1cc0cfc577a00be15abce98ee0942047e612295f26b42a022e982749fbe2138b6daada43755b2a7b888db0d43caf60e4cac7a587d532097324bf44f57fc75ba24b541f6c230c5fc829843e33d81e2a211e5765fdcfd17f01cc7351ce0a5aa58771d414fa05274ebfb36fa34baaa5a25ff6bb063e8d6ce621c0ea9088aab74de6326007dd05c846e9523d9e6d42b5dce339a3a517abaeceb5565c11f589d2529562d5038d246d0f1a0000000500000091b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c391b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+            let txBlobStr = "08054000f0c74a8500c5b8c96a648236886f8a92b96a9f52791ffc6ab801a034887ebf4f0080a01d8a0c982bb5168a76bc6387f6c5c07121b1708d2d6189fa4b4aa336113700ecf8fd4b9c748a7baf6d83933f4a470a394a4cb97375dc747be58222dae6fe660028ab7c39c82820284f6513a5709ce60db444127363fb62ea941a22bca777d95900a2a56d7e838653933fd36e5a0a307bd9c1fc196af9ed29eacfe31ea41540776600e63166b3416619b8fc5501054ba6ccabacf8168e221363ec7e12c95ea66b7623000ef4bd969740ccc4a860d2b60e2df766f92b075b985ca178ef5581c22681fe0b007ede40cfeb8592d7626347bfb8ebdfacaaaa3350c982d384d9ac6f183c0ab90c00be7b7b9260071da18923cbeb5ba56c8df53490c80baaf8f7a53b17756ec6ed0a008ab9ee1d7ddd0a41918baba2221b63758195cdef662e94cb1b0410bfad8da37e000e95f6bf1d64fb2765af2202a8f262f7e02d49c2d7a55de644dcbb26e658522f00365cb5c9480ab97bf756d4d14109e43285717d3e47073491af6134bd027bdc06008e97da96e2323b8ee4976da07cd1514bf4a7a123bdab1e2a74aec405d6ca644600f6ada0eec0bf9b898e8d51f5f3939a6b3cf2e0e17ed5b778c6002e6e97c1af0100dc12f0393490fc9254cbeb68d57151687927721f2bb751aed8e41f8bd47fd41100ea5a4502661aa5acfffff661832590c0d99b1e0c9a0f6fdcac4c00e58080e771d50391010b63ce64c10c05dd0700000500000012fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c712fddc9e2128b3fe571e4e5427addcb87fcaf08493867a68dd6ae44b406b39c7";
 
             const txBlob = Buffer.from(txBlobStr, "hex");
 
@@ -301,7 +305,7 @@ describe('SR25519', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", "s-sign_large_nomination", 34);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_large_nomination_sr25519`, model === "nanos" ? 34 : 19);
 
             let signatureResponse = await signatureRequest;
             console.log(signatureResponse);
