dd0c5d0b98b9870238f0b669da88fc05a59731e0
Juan Leni <lenijuan@gmail.com>
2021-03-25 11:32:50 +0100

    temporaritly desactivate prev version / update test vectors

--- tests/ui_tests.cpp
+++ tests/ui_tests.cpp
@@ -162,6 +162,6 @@ INSTANTIATE_TEST_SUITE_P
 TEST_P(JsonTestsA, CheckUIOutput_CurrentTX_Normal) { check_testcase(GetParam(), false); }
 TEST_P(JsonTestsA, CheckUIOutput_CurrentTX_Expert) { check_testcase(GetParam(), true); }
 
-// Parametric test using previous runtime:
-TEST_P(JsonTestsB, CheckUIOutput_PreviousTX_Normal) { check_testcase(GetParam(), false); }
-TEST_P(JsonTestsB, CheckUIOutput_PreviousTX_Expert) { check_testcase(GetParam(), true); }
+//// Parametric test using previous runtime:
+//TEST_P(JsonTestsB, CheckUIOutput_PreviousTX_Normal) { check_testcase(GetParam(), false); }
+//TEST_P(JsonTestsB, CheckUIOutput_PreviousTX_Expert) { check_testcase(GetParam(), true); }
