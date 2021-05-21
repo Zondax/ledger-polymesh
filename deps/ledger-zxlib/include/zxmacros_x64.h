332407ab137b8b717fb80ddd8f9252e545f690b0
Juan Leni <lenijuan@gmail.com>
2021-05-21 13:51:16 +0200

    upgrade zxlib , dependencies and tests

--- deps/ledger-zxlib/include/zxmacros_x64.h
+++ deps/ledger-zxlib/include/zxmacros_x64.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-*  (c) 2019 Zondax GmbH
+*   (c) 2018 Zondax GmbH
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
@@ -14,5 +14,25 @@
 *  limitations under the License.
 ********************************************************************************/
 #pragma once
+
+#if !defined (TARGET_NANOS) && !defined(TARGET_NANOX)
+
+#define MEMMOVE memmove
+#define MEMSET memset
+#define MEMCPY memcpy
+#define MEMCMP memcmp
+#define MEMCPY_NV memcpy
+
+#define PIC(x) (x)
+#define CHECK_APP_CANARY() {}
+//#define CX_ECCINFO_PARITY_ODD 1u
+//#define CX_ECCINFO_xGTn 2u
+
+#ifndef __APPLE__
+#define MEMZERO explicit_bzero
+#else
+__Z_INLINE void __memzero(void *buffer, size_t s) { memset(buffer, 0, s); }
+#define MEMZERO __memzero
+#endif
 
 #endif
